1f9db60ddd767d86f4feb3fed6d0ebcce9c54ba7
AliZad64 <78386181+AliZad64@users.noreply.github.com>
2021-09-04 18:42:08 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "bookmark-black": "#2D2926FF",
+        "bookmark-red": "#E94B3CFF",
+        "bookmark-blue": "#89ABE3FF",
+        "sailor-blue": "#00203FFF",
+        "bookmark-mint": "#ADEFD1FF",
+        "feature-color-form": "#f99b82",
+        "feature-color-calander": "#f1e0ba",
+        "feature-color-process": "#cae8e2",
+        "feature-color-users": "#15233e",
+      },
+    },
+    container: {
+      screens: {
+      lg: "1124px",
+      xl: "1124px",
+      "2xl": "1124px",
+    },
+  },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
